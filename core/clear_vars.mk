5d1cb291c3b35ac7d34b655897f5fe18b1af10d1
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    Add flamingo build support.
    Squashed also with the following changes:
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: Unconditionally use flamingo pathmap.
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca.stefani.ge1@gmail.com>
      Date:   Tue Jan 22 15:50:52 2019 +0100
    
        Don't export BUILD_NUMBER in envsetup.
    
        * The value is meant to be set only before running an /official/ build
        * This allows us to execute consecutive make commands without
          running kati for every invocation
    
      Change-Id: Ifa94b839f30c7260009c8a3c91c202b50e28022e
    
    Change-Id: I6ca3c6a68aa5ea78649b96ac86fb65ff7f489736

--- core/clear_vars.mk
+++ core/clear_vars.mk
@@ -518,7 +518,8 @@ LOCAL_TEST_PACKAGE :=
 full_android_manifest :=
 non_system_module :=
 
-module_license_metadata :=
+# Include any vendor specific clear_vars.mk file
+-include vendor/*/build/core/clear_vars.mk
 
 # Trim MAKEFILE_LIST so that $(call my-dir) doesn't need to
 # iterate over thousands of entries every time.
